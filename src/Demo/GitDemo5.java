e7bc0f665c093a744397259a7205ec0c05a5c7de
Martha <Martha@10.0.0.245>
2021-09-30 21:13:07 -0400

    save changes

--- src/Demo/GitDemo5.java
+++ src/Demo/GitDemo5.java
@@ -3,6 +3,13 @@ package Demo;
 public class GitDemo5 {
 	public static void main(String[] args) {
 		System.out.println("Git Demo Five....");
+		
+		boolean isSunday = true;
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("XYZ");
+		}
 	}
 
 }
